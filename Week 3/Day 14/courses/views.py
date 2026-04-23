bba9e9ac588ef97aff7479f6eff4f0a371c317b1
Aghin <aghinjoseaji02@gmail.com>
2026-04-23 13:16:37 +0530

    Day 14 - Models & Database  completed

--- Week 3/Day 14/courses/views.py
+++ Week 3/Day 14/courses/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Course
 from django.http import HttpResponse
 
 def home_view(request):
@@ -8,9 +9,12 @@ def detail_view(request, name):
     return HttpResponse(f"This is course: {name}")
 
 def course_list(request):
+    courses = Course.objects.all()
+    
     context = {
         'title': 'Available Courses',
         'user_name': 'Aghin',
-        'courses': ['Python', 'Django', 'HTML', 'CSS']
+        'courses': courses
     }
+
     return render(request, 'courses/course_list.html', context)
