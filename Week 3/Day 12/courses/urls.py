bba2bcc706056ea28678b425e01d9462aac60549
Aghin <aghinjoseaji02@gmail.com>
2026-04-23 13:01:07 +0530

    Day 12 MVT Architecture & URL Routing completed

--- Week 3/Day 12/courses/urls.py
+++ Week 3/Day 12/courses/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.course_list),  
-    path('course/<str:name>/', views.detail_view),
+    path('', views.home),
 ]
--- Week 3/Day 12/courses/views.py
+++ Week 3/Day 12/courses/views.py
@@ -1,16 +1,4 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
-def home_view(request):
-    return HttpResponse("Welcome to Klaw Courses")
-
-def detail_view(request, name):
-    return HttpResponse(f"This is course: {name}")
-
-def course_list(request):
-    context = {
-        'title': 'Available Courses',
-        'user_name': 'Aghin',
-        'courses': ['Python', 'Django', 'HTML', 'CSS']
-    }
-    return render(request, 'courses/course_list.html', context)
+def home(request):
+    return HttpResponse("Hello Day 12")
